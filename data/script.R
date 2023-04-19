59883e92e24b5da180acb68e2da38346d5a2e9ec
Rodslater <91638825+Rodslater@users.noreply.github.com>
2023-04-19 17:22:36 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -6,16 +6,14 @@ site21 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2023/r
 
 link21 <- site21 %>% html_nodes(xpath="//a[contains(text(), '.csv')]") %>% html_attr("href")
 
-destino21 <- "SRAG21.csv”
+destino21 <- "SRAG21.csv"
 
 Map(function(u, d) download.file(u, d, mode="wb"), link21, destino21)
-
 
 site22 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2023/resource/62803c57-0b2d-4bcf-b114-380c392fe825")
 
 link22 <- site22 %>% html_nodes(xpath="//a[contains(text(), '.csv')]") %>% html_attr("href")
-
-destino22<- "SRAG22.csv”
+destino22<- "SRAG22.csv"
 
 Map(function(u, d) download.file(u, d, mode="wb"), link22, destino22)
 
@@ -23,12 +21,11 @@ site23 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2023/r
 
 link23 <- site23 %>% html_nodes(xpath="//a[contains(text(), '.csv')]") %>% html_attr("href")
 
-destino23<- "SRAG23.csv”
+destino23<- "SRAG23.csv"
 
 Map(function(u, d) download.file(u, d, mode="wb"), link23, destino23)
 
 SRAG <- import_list(dir(pattern = ".csv"), rbind = TRUE, encoding = "UTF-8")
-
 
 arquivos_csv <- dir(pattern = ".csv")
 file.remove(arquivos_csv)
