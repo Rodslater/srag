7d843f7c52922d7496cda0e423965cde81d1fe08
Rodslater <91638825+Rodslater@users.noreply.github.com>
2023-04-20 07:47:17 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -27,7 +27,24 @@ SRAG <- SRAG |>
   filter(SG_UF_NOT == "SE") |>  
   mutate(data = dmy(DT_NOTIFIC),
          data_evolucao = dmy(DT_EVOLUCA),
-         idoso = ifelse(NU_IDADE_N >= 60, 'Sim','Não')) |> 
+         idoso = ifelse(NU_IDADE_N >= 60, 'Sim','Não'),
+         grupo_idade = case_when(idade < 5 ~ "0 a 4 anos",
+                                      idade > 4 & idade < 10 ~ "05 a 9 anos",
+                                      idade > 9 & idade < 15 ~ "10 a 14 anos",
+                                      idade > 14 & idade < 20 ~ "15 a 19 anos",
+                                      idade > 19 & idade < 25 ~ "20 a 24 anos",
+                                      idade > 24 & idade < 30 ~ "25 a 29 anos",
+                                      idade > 29 & idade < 35 ~ "30 a 34 anos",
+                                      idade > 34 & idade < 40 ~ "35 a 39 anos",
+                                      idade > 39 & idade < 45 ~ "40 a 44 anos",
+                                      idade > 44 & idade < 50 ~ "45 a 49 anos",
+                                      idade > 49 & idade < 55 ~ "50 a 54 anos",
+                                      idade > 54 & idade < 60 ~ "55 a 59 anos",
+                                      idade > 59 & idade < 65 ~ "60 a 64 anos",
+                                      idade > 64 & idade < 70 ~ "65 a 69 anos",
+                                      idade > 69 & idade < 75 ~ "70 a 74 anos",
+                                      idade > 74 & idade < 80 ~ "75 a 79 anos",
+                                      idade > 79 ~ "80 anos ou mais")) |> 
   select(data, semana=SEM_NOT,codigo=CO_MUN_NOT, sexo=CS_SEXO, raca=CS_RACA, idade=NU_IDADE_N, idoso,
          vacina_covid=VACINA_COV, classificacao=CLASSI_FIN, evolucao=EVOLUCAO, data_evolucao) |> 
     arrange(data)
