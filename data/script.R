e79551a967ecc25890bc235f0d32959bb53dbf70
Rodslater <91638825+Rodslater@users.noreply.github.com>
2024-04-10 09:41:00 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -4,21 +4,27 @@ library(tidyverse)
 library(sf)
 
 
-site21 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2023/resource/dd91a114-47a6-4f21-bcd5-86737d4fc734")
+site21 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2024/resource/dd91a114-47a6-4f21-bcd5-86737d4fc734")
 link21 <- site21 %>% html_nodes(xpath="//a[contains(text(), '.csv')]") %>% html_attr("href")
 destino21 <- "SRAG21.csv"
 Map(function(u, d) download.file(u, d, mode="wb"), link21, destino21)
 
-site22 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2023/resource/62803c57-0b2d-4bcf-b114-380c392fe825")
+site22 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2024/resource/62803c57-0b2d-4bcf-b114-380c392fe825")
 link22 <- site22 %>% html_nodes(xpath="//a[contains(text(), '.csv')]") %>% html_attr("href")
 destino22<- "SRAG22.csv"
 Map(function(u, d) download.file(u, d, mode="wb"), link22, destino22)
 
-site23 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2023/resource/0d78ff63-d6ca-4311-8dc8-6123cf1ca127")
+site23 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2024/resource/0d78ff63-d6ca-4311-8dc8-6123cf1ca127")
 link23 <- site23 %>% html_nodes(xpath="//a[contains(text(), '.csv')]") %>% html_attr("href")
 destino23<- "SRAG23.csv"
 Map(function(u, d) download.file(u, d, mode="wb"), link23, destino23)
 
+site24 <- read_html("https://opendatasus.saude.gov.br/dataset/srag-2021-a-2024/resource/8cb52f73-0184-41d5-8a8f-87d8f415652c")
+link24 <- site24 %>% html_nodes(xpath="//a[contains(text(), '.csv')]") %>% html_attr("href")
+destino24<- "SRAG24.csv"
+Map(function(u, d) download.file(u, d, mode="wb"), link24, destino24)   
+
+    
 SRAG <- import_list(dir(pattern = ".csv"), rbind = TRUE, encoding = "UTF-8")
 
 arquivos_csv <- dir(pattern = ".csv")
